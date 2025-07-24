0aeb5c1eca92eec1125ee2edf05af15d078899b8
Lina <linamered3@gmail.com>
2025-07-23 23:10:31 -0400

    Added text to second HStack, changed text for both, add text in Vstack on top of both HStacks, added background color in ZStack, added backgrounds with color on both HStacks

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -10,30 +10,55 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color(.blue)
+            Color(red: 118/225, green: 104/225, blue: 158/225)
                 .cornerRadius(20)
                 .padding()
-            VStack(spacing: 20.0) {
+            
+            VStack(spacing: 30.0) {
+
+                Text("Lina M")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .padding()
+                
                 HStack {
                     Image("hooping")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .cornerRadius(20)
                         .padding()
-                    
-                    
-                    Text("Lina M")
-                        .font(.title)
+                        
+                    Text("I love playing basketball! I play for my school and on an AAU team.")
+                        .font(.body)
                         .fontWeight(.bold)
                         .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        
+                        
                 }
+                .padding(10)
+                .background(Rectangle() .foregroundColor(Color(red: 95/225, green: 80/225, blue: 135/225)))
+                .cornerRadius(20)
+                .padding(8)
+                
                 HStack {
+                    Text("I'm a rising sophomore in northern Virginia!")
+                        .font(.body)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                    
                     Image("aboutmepic")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        
-                        
+                        .cornerRadius(20)
+                        .padding()
                 }
+                .padding(10)
+                .background(Rectangle() .foregroundColor(Color(red: 95/225, green: 80/225, blue: 135/225)))
+                .cornerRadius(20)
+                
             }
             .padding()
             
